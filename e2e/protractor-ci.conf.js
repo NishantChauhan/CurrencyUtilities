a8ea2772af4f5b5752d7bf9d5c6cbee854a5be60
Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>
2020-06-04 07:56:10 -0400

    Added new tests and setup new jobs in continuous deployment
      * Improvements
        1. Added new jobs for End to End Test of production post deployment
        2. Added test for cache response validation
        3. Added names to the Continous Integration steps in Github Actions
    
    Signed-off-by: Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,7 +1,7 @@
 const config = require('./protractor.conf').config
 const ciMultiBrowsers = require('./browserConfig').ciMultiBrowsers
 
-config.capabilities = ciMultiBrowsers
+config.multiCapabilities = ciMultiBrowsers
 config.directConnect = true
 config.chromeDriver = '/usr/bin/chromedriver'
 config.useAllAngular2AppRoots = true
