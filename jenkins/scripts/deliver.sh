b363b6300d4095a2dceb43dbdd8efbba47e09b9f
Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>
2020-06-10 07:46:50 -0400

    Added improvements to support Jenkins as well as GitHub actions.
      * Changes
        1) Added build file for building angular distribution
    	2) Added parameter for app URL for production test
        3) Parameterized backend URL for build and jenkins CI support
    
    Signed-off-by: Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 docker login -u $DOCKER_HUB_CREDS_USR -p $DOCKER_HUB_CREDS_PSW
 
-docker build -t nishantchouhan/currency-utilities-app:latest  .
-docker tag nishantchouhan/currency-utilities-app:latest nishantchouhan/currency-utilities-app:$BUILD_NUMBER
+docker build --build-arg BACK_END_API_ENDPOINT=http://localhost:5000/api/v1 -t nishantchouhan/currency-utilities-app:latest  .
+docker tag nishantchouhan/currency-utilities-app:latest nishantchouhan/currency-utilities-app:jenkins-$BUILD_NUMBER
 docker push nishantchouhan/currency-utilities-app:latest
-docker push nishantchouhan/currency-utilities-app:${BUILD_NUMBER}
+docker push nishantchouhan/currency-utilities-app:jenkins-${BUILD_NUMBER}
 docker logout
